421297ee22d7691bc120d5617c9762c59b9374ea
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-05-26 18:38:48 +1200

    Init Next.js client

---
+++ client/config/site.ts
@@ -0,0 +1,18 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Mood Matrix",
+  description:
+    "Mood Matrix - Your Productivity App for Work Day Sentiment Analysis.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/shadcn",
+    github: "https://github.com/brijeshbv/mood-matrix",
+    docs: "https://moodmatrix.com/docs",
+  },
+}
