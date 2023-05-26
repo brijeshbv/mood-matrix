7939e084635f1a3fd9b087e42d9cde5df33f08db
Allen Geer <allen.geer@provoke.co.nz>
2023-05-26 16:18:51 +1200

    Adding the LLM analysis methods and git log

---
+++ matrix/tests/test_sentiment.py
@@ -0,0 +1,20 @@
+import pytest
+from api import llmbox
+
+def test_get_sentiment_positive():
+    content = "I love this product! It's amazing."
+    data = {'content': content}
+    sentiment = llmbox.get_sentiment(data)
+    assert sentiment == 'positive'
+
+def test_get_sentiment_negative():
+    content = "This movie is terrible. I hated it."
+    data = {'content': content}
+    sentiment = llmbox.get_sentiment(data)
+    assert sentiment == 'negative'
+
+def test_get_sentiment_neutral():
+    content = "This book is okay. Not too good, not too bad."
+    data = {'content': content}
+    sentiment = llmbox.get_sentiment(data)
+    assert sentiment == 'neutral'
