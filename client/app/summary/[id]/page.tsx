5fa611b1c081ae02e456af9315b7d3474f973974
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-05-26 19:51:38 +1200

    Dynamic route for summary page

--- client/app/summary/[id]/page.tsx
+++ client/app/summary/[id]/page.tsx
@@ -1,8 +1,8 @@
 export default function Page({ params }: { params: { id: string } }) {
   console.log(params.id)
   return (
-    <div className="flex flex-1 flex-row">
-      <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
+    <div className="flex flex-1 flex-row text-center">
+      <h1 className="text-3xl font-extrabold leading-tight tracking-tighter  sm:text-3xl md:text-5xl lg:text-6xl">
         Summary for {params.id}
       </h1>
     </div>
