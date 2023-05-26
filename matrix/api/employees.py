383f1b62789c6367b0837a318c52f6d59ba75054
Allen Geer <allen.geer@provoke.co.nz>
2023-05-26 16:58:19 +1200

    Added summary and sentiment

--- matrix/api/employees.py
+++ matrix/api/employees.py
@@ -4,7 +4,8 @@ from matrix.db import get_users
 from flask_cors import CORS
 from matrix.api.utils import expect
 from datetime import datetime
-
+import json
+import matrix.api.llmbox
 
 mood_mtx_api_v1 = Blueprint(
     'mood_mtx_api_v1', 'mood_mtx_api_v1', url_prefix='/api/v1')
@@ -16,4 +17,25 @@ def api_get_users():
     users = get_users(20)
     return jsonify(users)
 
+
+@mood_mtx_api_v1.route('/sentiment/<email>', methods=['GET'],  defaults={'year':None, 'month': None, 'day': None})
+@mood_mtx_api_v1.route('/sentiment/<email>/<year>', methods=['GET'],  defaults={ 'month': None, 'day': None})
+@mood_mtx_api_v1.route('/sentiment/<email>/<year>/<month>', methods=['GET'],  defaults={ 'day': None})
+@mood_mtx_api_v1.route('/sentiment/<email>/<year>/<month>/<day>', methods=['GET'])
+def get_sentiment(email, month, day, year):
+    gitcommit = json.load(open("matrix/api/git_log.json"))
+    commits = gitcommit[email]
+    if len(commits) > 0:
+        return matrix.api.llmbox.get_sentiment(commits[0])
+
+
+@mood_mtx_api_v1.route('/summary/<email>', methods=['GET'],  defaults={'year':None, 'month': None, 'day': None})
+@mood_mtx_api_v1.route('/summary/<email>/<year>', methods=['GET'],  defaults={ 'month': None, 'day': None})
+@mood_mtx_api_v1.route('/summary/<email>/<year>/<month>', methods=['GET'],  defaults={ 'day': None})
+@mood_mtx_api_v1.route('/summary/<email>/<year>/<month>/<day>', methods=['GET'])
+def get_summary(email, month, day, year):
+    gitcommit = json.load(open("matrix/api/git_log.json"))
+    commits = gitcommit[email]
+    if len(commits) > 0:
+        return matrix.api.llmbox.get_summary(commits[0])
 
