9d3dc9ae0e33b3708ec1cf1b102e2a30a5d08d3f
Allen Geer <allen.geer@provoke.co.nz>
2023-05-26 20:41:22 +1200

    Add redis cache.

--- matrix/api/llmbox.py
+++ matrix/api/llmbox.py
@@ -4,11 +4,21 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.chains.mapreduce import MapReduceChain
 from langchain.prompts import PromptTemplate
 from langchain.docstore.document import Document
+import hashlib, json
 
 from dotenv import load_dotenv
-
+import redis, os
 load_dotenv()
+cache = redis.StrictRedis(host=os.environ['REDIS_HOST'],port=6380,db=0,password=os.environ['REDIS_PASS'],ssl=True)
+
+
 def get_sentiment(items):
+    md5=hashlib.md5()
+    md5.update(json.dumps(items, sort_keys=True).encode())
+    cachekey = f"sentiment.{md5.hexdigest()}"
+    cacheval = cache.get(cachekey)
+    if cacheval:
+        return json.loads(cacheval)
     prompt = """
         Summarise the sentiment of the text as either positive, neutral, negative.  Give extreme weight to negativity.
         
@@ -35,9 +45,16 @@ def get_sentiment(items):
 
     resp = llm_chain({"input_documents": docs}, return_only_outputs=True)#.get('output_text').strip()#llm_chain(item["content"])["text"].strip().lower()
     resp["items"] = items
+    cache.set(cachekey, json.dumps(resp, sort_keys=True))
     return resp
 
 def get_coaches(items):
+    md5 = hashlib.md5()
+    md5.update(json.dumps(items, sort_keys=True).encode())
+    cachekey = f"coaches.{md5.hexdigest()}"
+    cacheval = cache.get(cachekey)
+    if cacheval:
+        return json.loads(cacheval)
     prompt = """
         Please provide critical feedback on the communication below and be a touch condescending that is intended to make the developer a better team mate.
 
@@ -54,6 +71,7 @@ def get_coaches(items):
     resp = chain({"input_documents": docs},
                      return_only_outputs=True)  # .get('output_text').strip()#llm_chain(item["content"])["text"].strip().lower()
     resp["items"] = items
+    cache.set(cachekey, json.dumps(resp, sort_keys=True))
     return resp
 
 def get_summaries(items):
@@ -66,6 +84,12 @@ def get_summaries(items):
         Returns:
             str: The generated summary for the content.
     """
+    md5 = hashlib.md5()
+    md5.update(json.dumps(items, sort_keys=True).encode())
+    cachekey = f"summary.{md5.hexdigest()}"
+    cacheval = cache.get(cachekey)
+    if cacheval:
+        return json.loads(cacheval)
     prompt = """
             Please summarise the contributions made.
 
@@ -82,6 +106,7 @@ def get_summaries(items):
     resp = chain({"input_documents": docs},
                      return_only_outputs=True)  # .get('output_text').strip()#llm_chain(item["content"])["text"].strip().lower()
     resp["items"] = items
+    cache.set(cachekey, json.dumps(resp, sort_keys=True))
     return resp
 
 
