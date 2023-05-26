c0972303d09a27c05806bf8be3817fbd9ff99248
Brijesh Vishwanath <brijesh.163@gmail.com>
2023-05-27 10:47:25 +1200

    removed junk data

--- client/app/summary/page.tsx
+++ client/app/summary/page.tsx
@@ -14,38 +14,6 @@ const fetcher = (url) => fetch(url, {
   console.log(res)
   return res.json()});
 
-const data: Payment[] = [
-  {
-    id: "m5gr84i9",
-    amount: 316,
-    rating: "1",
-    email: "ken99@yahoo.com",
-  },
-  {
-    id: "3u1reuv4",
-    amount: 242,
-    rating: "3",
-    email: "Abe45@gmail.com",
-  },
-  {
-    id: "derv1ws0",
-    amount: 837,
-    rating: "2",
-    email: "Monserrat44@gmail.com",
-  },
-  {
-    id: "5kma53ae",
-    amount: 874,
-    rating: "4",
-    email: "Silas22@gmail.com",
-  },
-  {
-    id: "bhqecj4p",
-    amount: 721,
-    rating: "5",
-    email: "carmella@hotmail.com",
-  },
-]
 
 export default function DataTableDemo() {
 
