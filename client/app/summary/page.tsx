9d07e602f25b958c3160602cdfbb95af317dd068
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-05-27 10:36:31 +1200

    Summary table

--- client/app/summary/page.tsx
+++ client/app/summary/page.tsx
@@ -1,58 +1,49 @@
 "use client"
 import Link from "next/link"
 
-import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
-
-import { Payment, columns } from "@/components/column"
-import { DataTable } from "@/components/data-table"
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 import useSWR from 'swr';
 
-const fetcher = (url) => fetch(url, {
+const fetcher = (url: string) => fetch(url, {
   method: 'GET'
 }).then((res) => { 
   return res.json()});
- 
-const data: Payment[] = [
-  {
-    id: "m5gr84i9",
-    amount: 316,
-    rating: "1",
-    email: "ken99@yahoo.com",
-  },
-  {
-    id: "3u1reuv4",
-    amount: 242,
-    rating: "3",
-    email: "Abe45@gmail.com",
-  },
-  {
-    id: "derv1ws0",
-    amount: 837,
-    rating: "2",
-    email: "Monserrat44@gmail.com",
-  },
-  {
-    id: "5kma53ae",
-    amount: 874,
-    rating: "4",
-    email: "Silas22@gmail.com",
-  },
-  {
-    id: "bhqecj4p",
-    amount: 721,
-    rating: "5",
-    email: "carmella@hotmail.com",
-  },
-]
 
-export default function DataTableDemo() {
+export default function Summary() {
 
   const {data, error } = useSWR(
  'http://127.0.0.1:5000/api/v1/users_rating', fetcher)
 
+ console.log(data)
+
   if (error) return <div>failed to load</div>
   if (!data) return <div>loading...</div>
 
-  return <DataTable columns={columns} data={data} />
+  return (
+
+  <Table>
+  <TableCaption>A list of your recent invoices.</TableCaption>
+  <TableHeader>
+    <TableRow>
+      <TableHead >Email</TableHead>
+      <TableHead >Rating</TableHead>
+    </TableRow>
+  </TableHeader>
+  <TableBody>
+    {data.map((user: any) => (
+      <TableRow key={user.email}>
+        <TableCell><Link href={`/summary/${user.email}`}>{user.email}</Link></TableCell>
+        <TableCell>{user.rating}</TableCell>
+        </TableRow>
+    ))}
+  </TableBody>
+</Table>)
 }
--- client/components/column.tsx
+++ client/components/column.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { ColumnDef } from "@tanstack/react-table"
+import Link from "next/link"
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -19,6 +20,18 @@ export const columns: ColumnDef<Payment>[] = [
   {
     accessorKey: "rating",
     header: "Rating",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const payment = row.original
+ 
+      return (
+        <Link href="/">
+          
+        </Link>
+      )
+    },
   },
 
 ]
