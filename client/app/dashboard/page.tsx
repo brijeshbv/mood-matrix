5a6c7053027ad71b6652447686077c52286068c1
Brijesh Vishwanath <brijesh.163@gmail.com>
2023-05-27 14:32:38 +1200

    reverted summary change

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -84,8 +84,8 @@ export default function Summary() {
   <TableBody>
     {data.data.map((user: any) => (
       <TableRow key={user.email}>
-        <TableCell className="text-blue-500 hover:text-blue-700 hover:underline"><Link href={`/summary/${user.email}`}>{user.name}</Link></TableCell>
-        <TableCell className="text-blue-500 hover:text-blue-700 hover:underline"><Link href={`/summary/${user.email}`}>{user.email}</Link></TableCell>
+        <TableCell className="text-blue-500 hover:text-blue-700 hover:underline"><Link href={`/dashboard/${user.email}`}>{user.name}</Link></TableCell>
+        <TableCell className="text-blue-500 hover:text-blue-700 hover:underline"><Link href={`/dashboard/${user.email}`}>{user.email}</Link></TableCell>
         <TableCell>{user.rating}</TableCell>
         </TableRow>
     ))}
